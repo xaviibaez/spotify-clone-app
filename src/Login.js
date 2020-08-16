58844ce25164ec68ac1916772dd716ccb6bd6d40
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-08-16 17:40:48 +0200

    Update Login.js

---
+++ src/Login.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Login.css'
+import './App.css';
+
+function Login() {
+    return (
+        <div className="App">
+            {/* Spotify Logo */}
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQXzHLWLB33jSJ_0aUXcNbRIC_dhM4jKcGN6A&usqp=CAU"
+                alt=""
+            />
+
+            {/* Login with spotify*/}
+
+            <button>Login with Spotify</button>
+
+        </div>
+    )
+}
+
+export default Login
