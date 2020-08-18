1aa1c58800641cfa0c59437842d2ee7b5a87efa1
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-08-18 10:48:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify'
 
 function App() {
+  //Almacenar variable temporal, solo en memoria
+  const [token, setToken] = useState(null);
+
   //Recuperar el token de la URL del metodo
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -11,15 +14,28 @@ function App() {
     window.location.hash = "";
 
     //NO ENTIENDO DE DONDE SALE ACCESS TOKEN
-    const token = hash.access_token;
+    //_token es un token temporal
+    const _token = hash.access_token;
 
-    console.log("Token: ", token);
+    //Si hay token lo seteamos
+    if (_token) {
+      setToken(_token)
+    }
+
+    console.log("Token: ", _token);
   }, []);
 
   return (
     <div className="App">
-      {/* Lee el codigo de .Login */}
-      <Login />
+      {
+        //Si hay token vamos a la siguiente pagina
+        token ? (
+          <h1>Logged</h1>
+        ) : (
+          //Si no hay token volvemos al -> codigo de .Login
+          <Login />
+        )
+      }
     </div>
   );
 }
