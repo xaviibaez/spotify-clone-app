602d4077667e6a714336ca8a4a100e0bbd8ceb16
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-08-25 19:30:48 +0200

    sidebar options
    sidebar options

--- src/Siderbar.js
+++ src/Siderbar.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import "./Sidebar.css";
+import SidebarOption from './SidebarOption';
 
 function Siderbar() {
     return (
         <div className="sidebar">
         <h1>SIDEBAR</h1>
-        <img className="sidebar_logo"></img>
+        <img className="sidebar_logo" alt=""/>
+
+        <SidebarOption option="Home"/>
+        <SidebarOption option="Search"/>
+        <SidebarOption option="Your Library"/>
     </div>
     )
 }
