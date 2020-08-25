baeb5a246f0349c9a0f1036cde9e9def8f8d85a2
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-08-25 18:47:31 +0200

    player
    player

--- src/Player.js
+++ src/Player.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from 'react';
+import "./Player.css";
+import Sidebar from "./Siderbar";
+import Body from "./Body";
 
-function Player() {
+function Player({ spotify }) {
     //DATA LAYER -> Para solucionar el problem del PROP DRILLING
     //La DATA del DATA LAYER es recuperada usando REACT CONTEXT API
     //Configurado en Index.js
     return (
-        <div>
+        <div className="player">
             <h1>HOLA</h1>
+            <div className="player_body">
+                <Sidebar />
+                <Body />
+            </div>
         </div>
     )
 }
