1e480e6c0d84bc1981d44338f8bee6b381d834fc
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-08-25 18:47:22 +0200

    sidebar
    sidebar

---
+++ src/Siderbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Sidebar.css";
+
+function Siderbar() {
+    return (
+        <div className="sidebar">
+        <h1>SIDEBAR</h1>
+    </div>
+    )
+}
+
+export default Siderbar
