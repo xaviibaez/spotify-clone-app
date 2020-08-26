9f2187fcfeff653e6666601956400554f50ebe0a
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-08-26 22:46:41 +0200

    Update Siderbar.js
    icons

--- src/Siderbar.js
+++ src/Siderbar.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import "./Sidebar.css";
 import SidebarOption from './SidebarOption';
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
 function Siderbar() {
     return (
