b4d36fe0b5be6e7250e070fd30c545c931cd0df5
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-08-27 11:13:06 +0200

    Update Footer.js
    iconos

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
 import "./Footer.css";
+/* Iconos material UI */
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
 
 function Footer() {
     return (
         <div className="footer">
-            <h1>FOOTER</h1>
+            <div className="footer__left">
+                <p>Footer</p>
+            </div>
+            <div className="footer__center">
+                <p>Footer</p>
+            </div>
+            <div className="footer__right">
+                <p>Footer</p>
+            </div>
         </div>
     )
 }
