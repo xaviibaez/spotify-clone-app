04b5a3dd8dbd167163cbf433282cca023fe56e65
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-08-29 00:17:35 +0200

    Update Header.js
    header finished

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './Header.css';
+import { useDataLayerValue } from "./DataLayer";
 
 /* Iconos */
 import SearchIcon from "@material-ui/icons/Search";
 import { Avatar } from '@material-ui/core';
 
 function Header() {
+    //Recoger info de usuario
+    const[{user}, dispatch] = useDataLayerValue();
+
     return (
         <div className="header">
             <div className="header__left">
+                {/* Barra de busqueda */}
                 <SearchIcon />
                 <input 
                     placeholder="Hola Placeholder"
@@ -17,8 +22,8 @@ function Header() {
             </div>
             <div className="header__right">
                 {/* Icono de usuario */}
-                <Avatar src="" alt="RQ" />
-                <h4>Xavi</h4>
+                <Avatar src={user?.images[0].url} alt={user?.display_name} />
+                <h4>{user?.display_name}</h4>
             </div>
         </div>
     );
