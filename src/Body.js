4c4834e24d16274fa6bfbe559cc503e4371280d8
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-08-31 23:30:31 +0200

    Update Body.js
    {/* Detalle de la playlist */}

--- src/Body.js
+++ src/Body.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import "./Body.css";
 import Header from "./Header";
+import { useDataLayerValue } from "./DataLayer";
+
+// Iconos
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 function Body({spotify}) {
+    //Recoger info de playlist
+    const[{discover_weekly}, dispatch] = useDataLayerValue();
+
     return (
         <div className="body">
             <Header spotify={spotify} />
+
+            {/* Detalle de la playlist */}
+            <div className="body__info">
+                {/* Imagen */}
+                <img src={discover_weekly?.images[0].url} alt=""/>
+                {/* Texto */}
+                <div className="body__infoText">
+                    <p>{discover_weekly?.name}</p>
+                    <p>{discover_weekly?.description}</p>
+                </div>
+            </div>
+
+
+
         </div>
     );
 }
